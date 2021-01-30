cbf0a226f79e622c47c9fd2a19a7f8f0a1569b30
Thanaphoom Babparn <tp.babparn@gmail.com>
2021-01-30 21:39:34 +0700

    Add RestTemplate Bean

--- src/main/kotlin/dev/tpcoder/springbootkotlin/SpringBootKotlinApplication.kt
+++ src/main/kotlin/dev/tpcoder/springbootkotlin/SpringBootKotlinApplication.kt
@@ -2,10 +2,16 @@ package dev.tpcoder.springbootkotlin
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.client.RestTemplateBuilder
+import org.springframework.context.annotation.Bean
+import org.springframework.web.client.RestTemplate
 
 @SpringBootApplication
-class SpringBootKotlinApplication
+class SpringBootKotlinApplication {
+    @Bean
+    fun restTemplate(builder: RestTemplateBuilder): RestTemplate = builder.build()
+}
 
 fun main(args: Array<String>) {
-	runApplication<SpringBootKotlinApplication>(*args)
+    runApplication<SpringBootKotlinApplication>(*args)
 }
